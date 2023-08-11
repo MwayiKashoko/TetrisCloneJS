98a74da672f110edddb65dbfef0f246b1b6e953c
Mwayi Kashoko <39887498+MwayiKashoko@users.noreply.github.com>
2023-08-10 20:51:10 -0400

    Update Tetris.js

--- Tetris.js
+++ Tetris.js
@@ -992,7 +992,7 @@ window.onload = () => {
     }
 
     let titleScreenImage = new Image();
-    titleScreenImage.src = "Images/TitleScreen.png";
+    titleScreenImage.src = "Images/titleScreen.png";
     let title = new Image();
     title.src = "Images/tetris.svg";
 
